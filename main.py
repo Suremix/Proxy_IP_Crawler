a4db7ced3c4540639540c11a6e99ddbae5b287d5
Su <s1585003962@gmail.com>
2024-08-16 13:13:05 +0800

    服务器过期，保存

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 # 目前的打算是从快代理这个网站里边整一些给自己用
 
 import os
+import re
 import time
 import requests
+import json
+
+import numpy as np
 import pandas as pd
+
 from fake_useragent import UserAgent
 from lxml import etree
 
 
+# 输入一个url（比如kuaidaili的某一页的url）
+# 返回一个装有ip信息的df
 def get_ip_data_df_from_url(url):
     # 设置headers
     user_agent = UserAgent()
@@ -16,27 +23,45 @@ def get_ip_data_df_from_url(url):
 
     # 申请网页响应，先默认肯定能接通
     respone = requests.get(url, headers=headers)
-    print(url + "  State_Code:", respone.status_code)
+    if respone.status_code != 200:
+        print(url + "  State_Code:", respone.status_code)
 
     # 从响应里获取html源码
     html = etree.HTML(respone.text)
-    tr_list = html.xpath('//table[@class="table table-b table-bordered table-striped"]/tbody/tr')
-    data_title_list = ["IP", "PORT", "匿名度", "类型", "位置", "最后验证时间"]
+    script_list = html.xpath('//script')
+    data_idx = -1
+    for i in range(0, len(script_list)):
+        script = script_list[i]
+        content = str(script.text)
+        if "const fpsList" in content:
+            data_idx = i
+            break
+
+    if data_idx == -1:
+        print("没有找到data的script")
+
+    # 爬虫获取数据
+    data_script_str = str(script_list[data_idx].text)
+    data_str = re.findall(r"const fpsList = \[(.*)\];", data_script_str)[0]
+    data_list = re.findall(r"{(.*?)}", data_str)
+    data_list = ["{" + data + "}" for data in data_list]
+    data_dict_list = [eval(data) for data in data_list]
 
     # 获取从源码里获取特定的数据，存入df并返回
-    num_ip = len(tr_list)
+    num_ip = len(data_dict_list)
+    data_title_list = ["ip", "port", "last_check_time", "location"]
     ip_data_df = pd.DataFrame(index=range(0, num_ip), columns=data_title_list)
     for i in range(0, num_ip):
-        tr = tr_list[i]
+        data_dict = data_dict_list[i]
         for data_title in data_title_list:
-            xpath_cmd = './td[@data-title="{0}"]'.format(data_title)
-            ip_data_df.loc[i, data_title] = tr.xpath(xpath_cmd)[0].text
+            ip_data_df.loc[i, data_title] = data_dict[data_title]
 
     # 最后对验证时间的类型该一下，改成datetime
-    ip_data_df["最后验证时间"] = pd.to_datetime(ip_data_df["最后验证时间"])
+    ip_data_df["last_check_time"] = pd.to_datetime(ip_data_df["last_check_time"])
     return ip_data_df
 
 
+# 旧的代码，每次固定保存25页最新的ip，已经弃用
 def save_ip_data_from_kuaidaili_old(ip_type, num_page, output_folder, sleep_time=2):
     # 根据需求设置好需要进行爬虫的url以及爬取的页数
     if ip_type == "open":
@@ -74,6 +99,8 @@ def save_ip_data_from_kuaidaili_old(ip_type, num_page, output_folder, sleep_time
     ip_data_df.to_csv(output_path, index=False, encoding="utf_8_sig")
 
 
+# 输入要爬的ip类型、输出的文件夹、在没有遇到已经获取的了的ip的情况下爬多少页就停止、以及爬每一页暂停的时间
+# 程序将爬取的ip信息以csv的方式保存在output_folder下，文件名字固定
 def save_ip_data_from_kuaidaili(ip_type, output_folder, max_page_num=10, sleep_time=2):
     # 设置保存的路径
     file_name = "ip_" + ip_type + "_dataset.csv"
@@ -84,11 +111,7 @@ def save_ip_data_from_kuaidaili(ip_type, output_folder, max_page_num=10, sleep_t
     output_exist_flag = os.path.exists(output_path)
     if output_exist_flag is True:
         old_ip_data_df = pd.read_csv(output_path)
-        last_time = pd.to_datetime(old_ip_data_df.loc[0, "最后验证时间"])
-    
-    # 新建一个df用来存新的ip信息
-    data_title_list = ["IP", "PORT", "匿名度", "类型", "位置", "最后验证时间"]
-    ip_data_df = pd.DataFrame(columns=data_title_list)
+        last_time = pd.to_datetime(old_ip_data_df.loc[0, "last_check_time"])
 
     # 根据需求设置好需要进行爬虫的url
     if ip_type == "open":
@@ -99,9 +122,14 @@ def save_ip_data_from_kuaidaili(ip_type, output_folder, max_page_num=10, sleep_t
         url = ""
         print("Warring: ip_type is wrong!")
 
+    # 新建一个df用来存新的ip信息
+    ip_data_df = pd.DataFrame()
+
+    # 设置好用于循环的参数
     page_num = 1
     stop_flag = False
-    # 先打开第一页，获取tr列表，然后遍历列表
+
+    # 先打开第一页，获取df，然后遍历df
     # 当遇到的ip时间小于保存的最新时间，就停止
     # 然后遍历结束就翻页
     # 如果一直没有遇到（或者flag说没文件），但是翻页已经超过一定页数了，也停止
@@ -109,11 +137,16 @@ def save_ip_data_from_kuaidaili(ip_type, output_folder, max_page_num=10, sleep_t
         url_page = url + str(page_num) + "/"
         page_ip_data_df = get_ip_data_df_from_url(url_page)
 
+        # 如果发现啥都没获取到，就退出
+        if page_ip_data_df.shape[0] == 0:
+            print("没有获取到数据:", url_page)
+            break
+
         # 我有个想法，我可以获取page的df之后，先看时间有没有小于last_time的
         # 如果没有，就将整个df用concat加到总的df里
         # 如果发现有，就将之前的那些concat到总的df里
         for idx in page_ip_data_df.index:
-            if (output_exist_flag is True) and (page_ip_data_df.loc[idx, "最后验证时间"] < last_time):
+            if (output_exist_flag is True) and (page_ip_data_df.loc[idx, "last_check_time"] < last_time):
                 # 这里有个问题，就是假如idx为0，则idx-1就变为-1了
                 # 但是由于index里没有-1这一项，所有0:-1会啥都选不中
                 # 这正是程序所想要的，要是page的第一个ip就已经是不需要的了，那啥都选不中就会把page的df设为一个空的df
@@ -137,7 +170,51 @@ def save_ip_data_from_kuaidaili(ip_type, output_folder, max_page_num=10, sleep_t
     
     # 然后保存文件
     ip_data_df.to_csv(output_path, index=False, encoding="utf_8_sig")
+
+
+# 输入需要检查的ip的csv
+# 程序将检查每个ip的可用性，如果不可用则会将valid_flag改为0
+def check_IP_validation(file_path):
+    if os.path.exists(file_path) is False:
+        print("文件不存在！")
+        return
+
+    ip_data_df = pd.read_csv(file_path, dtype={"port": str})
+    good_ip_data_df = ip_data_df.loc[ip_data_df["valid_flag"] == 1, :]
+    for idx in good_ip_data_df.index:
+        ip = good_ip_data_df.loc[idx, "ip"]
+        port = good_ip_data_df.loc[idx, "port"]
+        proxies = {"http": "http://" + ip + ":" + port}
+
+        user_agent = UserAgent()
+        headers = {"User-Agent": user_agent.chrome}
+
+        url = "http://www.baidu.com"
+        try:
+            requests.get(url, headers=headers, proxies=proxies, timeout=5)
+        except Exception as error:
+            ip_data_df.loc[idx, "valid_flag"] = 0
+            print("进度: " + str(idx+1) + "/" + str(np.max(good_ip_data_df.index+1)))
+            print("异常IP: " + ip + ":" + port)
+            print("异常类型: ", error.__class__.__name__)
+            print("异常信息: ", error, end="\n\n")
+
+        time.sleep(2)
     
+    ip_data_df.to_csv(file_path, index=False, encoding="utf_8_sig")
+
+
+# 输入需要寻找的ip的csv
+# 程序会把其中valid_flag等于1的ip信息拿出来，保存成一个文件
+def find_and_save_valid_IP(file_path, output_folder):
+    ip_data_df = pd.read_csv(file_path)
+    good_ip_data_df = ip_data_df.loc[ip_data_df["valid_flag"] == 1, :]
+    
+    file_name = os.path.basename(file_path)
+    output_name = "good_" + file_name
+    output_path = os.path.join(output_folder, output_name)
+    good_ip_data_df.to_csv(output_path, index=False, encoding="utf_8_sig")
+
 
 if __name__ == "__main__":
     print(time.asctime())
@@ -145,9 +222,8 @@ if __name__ == "__main__":
     save_ip_data_from_kuaidaili("open", output_folder)
     save_ip_data_from_kuaidaili("anonymous", output_folder)
 
+    check_IP_validation("/root/myData/ip_open_dataset.csv")
+    check_IP_validation("/root/myData/ip_anonymous_dataset.csv")
 
-
-
-
-
-
+    find_and_save_valid_IP("/root/myData/ip_open_dataset.csv", output_folder)
+    find_and_save_valid_IP("/root/myData/ip_anonymous_dataset.csv", output_folder)
--- test.py
+++ test.py
@@ -1,8 +1,31 @@
+import requests
 import pandas as pd
+from fake_useragent import UserAgent
 
-ip_data_df = pd.read_csv("/root/myData/ip_open_dataset_old.csv")
-print(ip_data_df.shape)
+user_agent = UserAgent()
+headers = {
+    "User-Agent": user_agent.chrome,
+    "Connection": "close",
+}
 
-ip_data_df = ip_data_df.loc[0:-1, :]
-print(ip_data_df.shape)
+ip_data_df = pd.read_csv("/root/myData/good_ip_open_dataset.csv", dtype={"PORT": str})
+ip = ip_data_df.loc[1, "IP"]
+port = ip_data_df.loc[1, "PORT"]
+proxies = {
+    "http":"http://" + ip + ":" + port,
+    "https":"http://" + ip + ":" + port,
+}
+
+# url = "http://ip111.cn"
+url = "http://www.baidu.com"
+error_tuple = (requests.exceptions.ProxyError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout)
+
+try:
+    respone = requests.get(url, headers=headers, timeout=5)
+except error_tuple as e:
+    print("异常IP: " + ip + ":" + port)
+    print("异常信息: ", e)
+else:
+    print(respone.status_code)
+    print(respone.status_code == 200)
 
